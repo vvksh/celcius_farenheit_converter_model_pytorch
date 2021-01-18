346795ef06c613b09ad31f16ea5f5514d34d392e
Vivek Sah <vivek.sah32+github@gmail.com>
2021-01-18 12:18:00 -0800

    Use pytorch's grad feature for computing gradients

--- model.py
+++ model.py
@@ -4,18 +4,26 @@ import torch
 
 class Model:
     def __init__(self, loss_fn, grad_fn):
-        self.w = torch.ones(())
-        self.b = torch.zeros(())
+        self.params = torch.tensor([1.0, 1.0], requires_grad = True) # w, b
         self.loss_fn = loss_fn
-        self.grad_fn = partial(grad_fn, self.w, self.b)
 
     def forward(self, input):
-        return (self.w * input) + self.b
+        w, b = self.params
+        return (w * input) + b
 
-    def training_loop(self, n_epochs, learning_rate, input_tensor, label_tensor, interval_to_print=10):
+    def training_loop(self,
+                      n_epochs:int,
+                      learning_rate: float,
+                      input_tensor: torch.Tensor,
+                      label_tensor: torch.Tensor,
+                      interval_to_print: int = 10) -> (float, float):
         for epoch in range(n_epochs):
+            if self.params.grad is not None:
+                self.params.grad.zero_()
+
             t_p = self.forward(input_tensor)
             loss = self.loss_fn(t_p, label_tensor)
+            loss.backward()
             w_grad, b_grad = self.grad_fn(input_tensor, label_tensor, t_p)
             self.w = self.w - learning_rate * w_grad
             self.b = self.b - learning_rate * b_grad
